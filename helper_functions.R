2ed1e61fdfb4fe7f0a78ad39d72956813c2fe786
Jed Grabman <JBGrabman@gmail.com>
2026-05-01 14:58:58 -0400

    Update RegisteredPlayers.txt when changing player name

--- helper_functions.R
+++ helper_functions.R
@@ -1,7 +1,11 @@
 update_player_name = function(old_name, new_name, vs_ratings){
   if (!any(vs_ratings$player_results$Player == old_name)){
     warning(paste("No player named", old_name, "found. No changes made."))
+    return()
   }
   vs_ratings$player_results$Player[vs_ratings$player_results$Player == old_name] = new_name
   vs_ratings$update_history$Player[vs_ratings$update_history$Player == old_name] = new_name
+  registered_players = read.delim("RegisteredPlayers.txt", header = FALSE)
+  registered_players[registered_players == old_name] = new_name
+  write.table(registered_players, "RegisteredPlayers.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
 }
